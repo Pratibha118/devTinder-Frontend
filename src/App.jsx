682b32ccd8152834df75269cfa4af90013da903c
Pratibha <Pratibhakaushik001@gmail.com>
2025-11-29 17:28:14 +0530

    Install Tailwind, DaisyUI, react-router-dom and created basic route and a navbar in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+
 
 function App() {
 
   return (
     <>
-   
-      <h1>Hello World</h1>
-      
+      <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<div>base path</div>} />
+        <Route path="/login" element={<div>login path</div>} />
+        <Route path="/hello" element={<div>hello path</div>} />
+
+      </Routes>
+      </BrowserRouter>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline" >Hello World</h1>
     </>
   )
 }
